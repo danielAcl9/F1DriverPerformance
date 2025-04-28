1486f615ae7a9f0c44d2931fe4f8e82a81ccda5f
Daniel Amado <dafeamca99@gmail.com>
2025-04-27 20:57:05 -0500

    Example Athena SQL queries added

--- aws/athena_queries.sql
+++ aws/athena_queries.sql
@@ -18,3 +18,19 @@ FROM f1_pipeline_db.processed
 GROUP BY FullName
 ORDER BY TotalPositionGain DESC !--Or ASC for losses;
 LIMIT 5;
+
+!-- Get drivers that have won a race this season
+SELECT FullName, COUNT(*) AS Wins
+FROM f1_pipeline_db.processed
+WHERE "Gap to Winner" = 0
+GROUP BY FullName
+ORDER BY Wins DESC
+LIMIT 5;
+
+! -- Get top 5 drivers by average gap to winner in the season so far
+SELECT FullName, ROUND(AVG(CAST("Gap to Winner" AS DOUBLE)), 3) AS AvgGapToWinner
+FROM f1_pipeline_db.processed
+WHERE "Position" != 1
+GROUP BY FullName
+ORDER BY AvgGapToWinner ASC
+LIMIT 5;
