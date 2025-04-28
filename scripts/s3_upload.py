f9360b5277f7bed07501227969d660b571bd483b
Daniel Amado <dafeamca99@gmail.com>
2025-04-27 19:00:30 -0500

    Created S3 upload script

--- scripts/s3_upload.py
+++ scripts/s3_upload.py
@@ -1,2 +1,13 @@
 import boto3
 
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Variables
+bucket_name = 'f1-pipeline-daniel2025'
+local_file_path = 'data\\f1_2025_season_results.csv'
+s3_key = 'processed/f1_2025_data.csv'
+
+s3.upload_file(local_file_path, bucket_name, s3_key)
+
+print('File uploaded to S3 successfully')
