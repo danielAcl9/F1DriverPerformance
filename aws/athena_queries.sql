b36b1de32a3f3509107bc99e5876e26d830c1858
Daniel Amado <dafeamca99@gmail.com>
2025-04-27 20:33:02 -0500

    Added Sprint Races results to the data

--- aws/athena_queries.sql
+++ aws/athena_queries.sql
@@ -5,3 +5,16 @@ FROM f1_pipeline_db.processed
 GROUP BY FullName
 ORDER BY TotalPoints DESC
 LIMIT 5;
+
+!-- Teams ordered by total points in the season so far
+SELECT TeamName, SUM(Points) AS TotalPoints
+FROM f1_pipeline_db.processed
+GROUP BY TeamName
+ORDER BY TotalPoints DESC;
+
+!-- Top drivers by Positions Gained in the season so far
+SELECT FullName, SUM(PositionGainLoss) AS TotalPositionGain
+FROM f1_pipeline_db.processed
+GROUP BY FullName
+ORDER BY TotalPositionGain DESC !--Or ASC for losses;
+LIMIT 5;
